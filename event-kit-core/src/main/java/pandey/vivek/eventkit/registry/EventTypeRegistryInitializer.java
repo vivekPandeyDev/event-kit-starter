4152a0e53dc25c096ab4d34405eff8d51b3967af
vivekPandeyDev <codewithvivekpandey@gmail.com>
2026-06-01 23:44:55 +0530

    feature: adding annotation for event type resolver and in-memory resolver

--- event-kit-core/src/main/java/pandey/vivek/eventkit/registry/EventTypeRegistryInitializer.java
+++ event-kit-core/src/main/java/pandey/vivek/eventkit/registry/EventTypeRegistryInitializer.java
@@ -2,31 +2,43 @@ package pandey.vivek.eventkit.registry;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.context.ApplicationContext;
-import pandey.vivek.eventkit.annotation.EventHandler;
+import org.springframework.boot.autoconfigure.AutoConfigurationPackages;
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
+import org.springframework.core.type.filter.AnnotationTypeFilter;
 import pandey.vivek.eventkit.annotation.EventType;
-import pandey.vivek.eventkit.registry.service.EventTypeRegistry;
 
 @RequiredArgsConstructor
 @Slf4j
 public class EventTypeRegistryInitializer implements InitializingBean {
 
-	private final ApplicationContext context;
+	private final BeanFactory beanFactory;
 
 	private final InMemoryEventTypeRegistry registry;
 
 	@Override
-	public void afterPropertiesSet() {
+	public void afterPropertiesSet() throws ClassNotFoundException {
 		log.info("Event handler registration using annotation");
-		var beans = context.getBeansWithAnnotation(EventHandler.class);
-		beans.values().forEach(bean -> {
-			Class<?> clazz = bean.getClass();
-			EventType type = clazz.getAnnotation(EventType.class);
-			if (type != null) {
+		var scanner = new ClassPathScanningCandidateComponentProvider(false);
+
+		scanner.addIncludeFilter(new AnnotationTypeFilter(EventType.class));
+
+		var basePackages = AutoConfigurationPackages.get(beanFactory);
+
+		for (String basePackage : basePackages) {
+
+			var candidates = scanner.findCandidateComponents(basePackage);
+
+			for (var candidate : candidates) {
+
+				Class<?> clazz = Class.forName(candidate.getBeanClassName());
+
+				EventType type = clazz.getAnnotation(EventType.class);
+
 				registry.register(type.value(), clazz);
 			}
-		});
+		}
 	}
 
 }
