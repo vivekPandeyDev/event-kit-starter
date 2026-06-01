c0dc194ade53658ba5fdaebadaed5ada6c477253
vivekPandeyDev <codewithvivekpandey@gmail.com>
2026-06-01 23:45:21 +0530

    feature: generic consumer to map event

---
+++ event-kit-starter-test/src/main/java/pandey/vivek/runner/GenericKafkaConsumer.java
@@ -0,0 +1,29 @@
+package pandey.vivek.runner;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+import pandey.vivek.eventkit.registry.service.EventTypeRegistry;
+import tools.jackson.databind.ObjectMapper;
+
+@Component
+public class GenericKafkaConsumer {
+
+    private final ObjectMapper mapper;
+
+    private final EventTypeRegistry registry;
+
+    public GenericKafkaConsumer(ObjectMapper mapper, EventTypeRegistry registry) {
+        this.mapper = mapper;
+        this.registry = registry;
+    }
+
+    @KafkaListener(topics = "media-events")
+    public void consume(ConsumerRecord<String, String> record) {
+        String payload = record.value();
+        String eventType = new String(record.headers().lastHeader("eventType").value());
+        Class<?> clazz = registry.resolve(eventType);
+        Object event = mapper.readValue(payload, clazz);
+        System.out.println(event);
+    }
+}
--- event-kit-starter-test/src/main/java/pandey/vivek/runner/TestEvent.java
+++ event-kit-starter-test/src/main/java/pandey/vivek/runner/TestEvent.java
@@ -1,12 +1,14 @@
 package pandey.vivek.runner;
 
 import pandey.vivek.eventkit.annotation.EventTopic;
+import pandey.vivek.eventkit.annotation.EventType;
 import pandey.vivek.eventkit.api.DomainEvent;
 
 import java.util.UUID;
 
 @EventTopic("media-events")
-public record TestEvent(UUID eventId, UUID mediaId)implements DomainEvent {
+@EventType("media.uploaded")
+public record TestEvent(UUID eventId, UUID mediaId) implements DomainEvent {
 
     public TestEvent {
     }
